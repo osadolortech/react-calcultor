4c0d32c0a11655dba8840de96c729a189738ef0e
osadolortech <brightosadolor8@gmail.com>
2022-04-21 16:06:02 +0100

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import  './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+    const [result, setResult] = useState("")
+
+    const baseclick = (e) =>{
+        setResult(result.concat(e.target.name ))
+    }
+
+    const clear = () =>{
+        setResult("")
+    }
+
+    const backspace = () =>{
+        setResult(result.slice("",-1))
+    }
+
+    const evaluate = () =>{
+        setResult(eval(result).toString())
+    }
+   
+    return(
+        <div className="container">
+            <form >
+                <input className='input' type="text" value={result}/>
+            </form>
+
+            <div className="keypad">
+                <button name="1" onClick={baseclick}>1</button>
+                <button name="2" onClick={baseclick}>2</button>
+                <button name="3" onClick={baseclick}>3</button>
+                <button name="4" onClick={baseclick}>4</button>
+                <button name="5" onClick={baseclick}>5</button>
+                <button name="6" onClick={baseclick}>6</button>
+                <button name="7" onClick={baseclick}>7</button>
+                <button name="8" onClick={baseclick}>8</button>
+                <button name="9" onClick={baseclick}>9</button>
+                <button name="0" onClick={baseclick}>0</button>
+                <button name="+" onClick={baseclick}>+</button>
+                <button name="-" onClick={baseclick}>-</button>
+                <button name="*" onClick={baseclick}>X</button>
+                <button name="/" onClick={baseclick}>/</button>
+                <button name="=" onClick={evaluate}>=</button>
+                <button name="C" onClick={clear} id="clear">C</button>
+                <button name="CE" onClick={backspace} className="backspace">CE</button>
+                <button name="." onClick={baseclick}>.</button>
+
+
+            </div>
+        </div>
+    )
 }
-
-export default App;
+export default App
